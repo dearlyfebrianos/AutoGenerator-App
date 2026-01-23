a7fc299259b0ee482147fba4730e85c81e2e9f7e
dearlyfebrianos <dearlyfebrianoi@gmail.com>
2026-01-23 23:19:35 +0700

    sja

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import HomePage from "./pages/Homepage";
 
 const App = () => {
   const [activeMenu, setActiveMenu] = useState("home");
-
+  const sa = "as"
   return (
     <div className="min-h-screen bg-gradient-to-br from-blue-50 via-indigo-50 to-purple-50 dark:from-gray-900 dark:via-gray-800 dark:to-gray-900 transition-colors duration-300">
       <Header activeMenu={activeMenu} setActiveMenu={setActiveMenu} />
